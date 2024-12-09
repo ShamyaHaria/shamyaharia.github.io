bcb9330290fdbdb761d5b93254e2a5c5975afb39
Z3r0Gr4v1ty <99024751+ShamyaHaria@users.noreply.github.com>
2024-12-09 10:51:23 +0530

    Create Test.js

---
+++ Test.js
@@ -0,0 +1,8 @@
+// test.js
+
+// Print a simple message to the console
+console.log("This is a test script!");
+
+// Stamp the name Shamya Haria
+const stampName = "Shamya Haria";
+console.log(`This script was created by: ${stampName}`);
