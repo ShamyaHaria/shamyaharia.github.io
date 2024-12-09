968e891e3b9c2aa544919dacc450c1da27966fd9
Shamya <shamyaharia@gmail.com>
2024-12-09 10:53:12 +0530

    Delete Test.js

--- Test.js
+++
@@ -1,8 +0,0 @@
-// test.js
-
-// Print a simple message to the console
-console.log("This is a test script!");
-
-// Stamp the name Shamya Haria
-const stampName = "Shamya Haria";
-console.log(`This script was created by: ${stampName}`);
